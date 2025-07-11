db5d3743da121a517ee88f05c72db6cdb109de12
Utyfua <utyfua@gmail.com>
2025-07-11 13:06:59 +0200

    feat: add locale support for Korean(ko-KR)

---
+++ src/locale/ko-KR.ts
@@ -0,0 +1,28 @@
+import type enUS from './en-US'
+
+const locale: typeof enUS = {
+  communityUniverDataSync: {
+    protectedAlert: {
+      title: '이 셀은 보호되어 있습니다',
+      message: '이 셀을 편집할 수 없습니다',
+    },
+    formulaRejectAlert: {
+      title: '수식은 허용되지 않습니다',
+      message: '이 셀에서는 수식을 사용할 수 없습니다',
+    },
+  },
+  // https://github.com/dream-num/univer/blob/6498d15464c9b4eb832eef261275d8028b05397a/packages/sheets-data-validation-ui/src/locale/en-US.ts
+  dataValidation: {
+    panel: {
+      invalid: '유효하지 않음',
+    },
+    any: {
+      error: '이 셀의 내용이 유효성 검사 규칙을 위반합니다',
+    },
+    list: {
+      dropdown: '선택',
+    },
+  },
+}
+
+export default locale
